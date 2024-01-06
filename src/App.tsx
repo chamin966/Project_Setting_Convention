a522fe234b48d75cdd668c6a96478cffb22dd679
김민찬 <armin@gimminchan-ui-Macmini.local>
2024-01-07 01:38:53 +0900

    Style: 개발자 도구에서 클래스네임의 유무에 따라 어떻게 표출되는가 비교

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import styled from 'styled-components';
 function App() {
   return (
     <>
-      <StyledMain>리액트 프로젝트 초기 설정</StyledMain>
+      <StyledMain>
+        스타일드 컴포넌트 클래스 네임 없이 사용
+        <StyledList>
+          <StyledItem>클래스네임 없는 아이템1</StyledItem>
+          <StyledItem>클래스네임 없는 아이템2</StyledItem>
+          <StyledItem>클래스네임 없는 아이템3</StyledItem>
+        </StyledList>
+      </StyledMain>
+      <StyledMain className='home_main'>
+        스타일드 컴포넌트 클래스 네임 사용
+        <StyledList className='home_list'>
+          <StyledItem className='home_item'>클래스네임 있는 아이템1</StyledItem>
+          <StyledItem className='home_item'>클래스네임 있는 아이템2</StyledItem>
+          <StyledItem className='home_item'>클래스네임 있는 아이템3</StyledItem>
+        </StyledList>
+      </StyledMain>
     </>
   );
 }
 
 export default App;
 
-const StyledMain = styled.main`
-  background-color: red;
-`;
+const StyledMain = styled.main``;
+const StyledList = styled.ul``;
+const StyledItem = styled.li``;
